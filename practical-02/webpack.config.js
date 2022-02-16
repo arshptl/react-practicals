211b574f40194497fc0b95036f9463bcdaae4b0d
arshptl <arshpatel714@gmail.com>
2022-02-16 17:56:14 +0530

    Practical-2: React project from scratch

---
+++ practical-02/webpack.config.js
@@ -0,0 +1,60 @@
+// const path = require("path");
+// const webpack = require("webpack");
+
+// module.exports = {
+//     entry: "./src/index.js",
+//     mode: "development",
+//     output: {
+//         filename: "bundle.js",
+//         path: path.resolve("dist"),
+//         publicPath: "/",
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.(js|jsx)$/,
+//                 exclude: /node_modules/,
+//                 use: "babel-loader"
+//             },
+//             {
+//                 test: /\.css$/,
+//                 use: ['style-loader', 'css-loader']
+//             }
+//         ],
+//     },
+// }
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
