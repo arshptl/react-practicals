3cfed5074e2c08d91a5495eae74c0e310af4854b
arshptl <arshpatel714@gmail.com>
2022-03-11 12:17:23 +0530

    cherry picked practical-07 into practical-07-01

--- practical-05/user-list-react-app/src/App.js
+++ practical-05/user-list-react-app/src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
 import Layout from "./components/wrapper/Layout";
-import UsersPage from "./pages/UsersPage";
+import { Route, Routes } from "react-router-dom";
+
+const About = React.lazy(() => import("./pages/About"));
+const Home = React.lazy(() => import("./pages/Home"));
+const UsersPage = React.lazy(() => import("./pages/UsersPage"));
 
 function App() {
   return (
     <Layout>
-      <UsersPage/>
+      <React.Suspense fallback={<p>Loading page...</p>}>
+        <Routes>
+          <Route path="/" element={<UsersPage />} exact />
+          <Route path="/about" element={<About />} exact />
+          <Route path="/home" element={<Home />} exact />
+        </Routes>
+      </React.Suspense>
     </Layout>
   );
 }
